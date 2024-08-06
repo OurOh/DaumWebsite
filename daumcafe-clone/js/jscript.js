f864db5285161d554f42df1f295a6496b015e5bd
OurOh <ahn980321@gmail.com>
2024-08-06 11:42:00 +0900

    0806 commit

--- daumcafe-clone/js/jscript.js
+++ daumcafe-clone/js/jscript.js
@@ -7,14 +7,20 @@ $(function(){
     $(this).addClass("active"); 
   });
  
-  $(".btn-right").click(function(){
+  $(".btn-right").click(function(e){
+      e.preventDefault();
       fadeInOut();
   });
 
+  $(".btn-left").click(function(e){
+    e.preventDefault();
+    fadeOutIn();
+});
+
+   fadeInOut();
    setInterval(fadeInOut, 9000);
 
 }); // /.jquery
-
 
 let i = 0;
 function fadeInOut(){
@@ -24,11 +30,12 @@ function fadeInOut(){
    $('.hero .hero-box:eq(0) ul>li:first-child a').addClass("active");
 }
 
-function fadeInOut(){
-  let ct = $(".hero-box").length;
-  console.log(ct);
-  $(".hero .her-box:eq("+(ct-1)+")").clone().prependTo('.hero');
-  $(".hero .hero-box:eq("+ct+")").remove();
-  $(".hero .hero-box").removerClass("act");
-  $('.hero .hero-box:eq(0) ul>li:first-child a').addClass("active");
+function fadeOutIn(){
+   let ct = $(".hero-box").length;
+   console.log(ct);
+   $(".hero .hero-box:eq("+(ct-1)+")").clone().prependTo('.hero');
+   $(".hero .hero-box:eq("+ct+")").remove();
+   $(".hero .hero-box").removeClass("act");
+   $(".hero .hero-box:eq(0)").addClass("act");
+   $('.hero .hero-box:eq(0) ul>li:first-child a').addClass("active");
 }
