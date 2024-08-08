f69b68ee358fd91a25e4ee5dc2e41013c5ab7206
OurOh <ahn980321@gmail.com>
2024-08-08 11:22:50 +0900

    0808 commit

--- daumcafe-clone/js/jscript.js
+++ daumcafe-clone/js/jscript.js
@@ -1,4 +1,12 @@
 $(function(){
+  let count = 0;
+  const now = new Date();
+  const nowMonth = (now.getMonth)
+  const nowDate = now.getFullYear() + "." + (now.getMonth()+1) + "." + now.getDate();
+  const nowTime = now.getHours() + ':' + now.getMinutes();
+
+
+  $('.text-date').html(nowDate + "<strong>" + nowTime + "</strong>");
   $(".grid-fill").hover(function(){
     $(this).find(".subnav").stop().fadeToggle();
   });
@@ -17,8 +25,36 @@ $(function(){
     fadeOutIn();
 });
 
+   $('.b-table').eq(0).css('display', 'flex');
    fadeInOut();
    setInterval(fadeInOut, 9000);
+
+   const bTableCount = $(".b-table").length;
+
+   $(document).on("click", ".run", function(){
+   //$('.run').click(function(){
+     if($(this).hasClass('btn-next')){
+         if(count > bTableCount - 2){
+           $('.btn-next').removeClass('run');
+         }else{
+              count++;
+              if(count > -1) {
+                 $('.btn-prev').addClass('run');
+              }
+         }   
+      }else{
+          if(count < 1){
+             $('.btn-prev').removeClass('run');
+          }else{
+             count--;
+             if(count <  bTableCount - 2){
+                $(".btn-next").addClass('run');
+             }
+          }
+      }       
+          $('.b-table').hide();
+          $('.b-table').eq(count).css('display', 'flex');
+   });
 
 }); // /.jquery
 
